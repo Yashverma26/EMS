089e99a5da65600fe6753896623ca3e0bd3f663a
Yashverma26 <yash.verrma2611@gmail.com>
2026-06-02 18:24:32 +0530

    new commit

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,9 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Message API");
+});
 app.use("/api/v1/message", messageRouter);
 
 dbConnection();
